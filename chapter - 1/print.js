66606bd3fe35134ca9733edb187fd3b0c080bf8f
Md Sakib hossen <akibhossainsakib1040@gmail.com>
2025-07-29 02:04:57 +0600

    completed 1 and a half chapter of ac-js one shot

---
+++ chapter - 1/print.js
@@ -0,0 +1,37 @@
+console.log("Hi,I am Sakib Hossen");
+console.log("I am From Daffodil International University");
+console.log("This is my first javascript Code Ever");
+
+var name = 'Sakib Hossen';
+console.log(typeof name);
+
+// Represents both integers and floating-point numbers.
+var price = 150.50 ;
+console.log(price);
+
+// Represents textual data enclosed in single or double quotes.
+var firstName = 'Taslima';
+var lastName ='Nasrin';
+
+console.log(firstName + lastName);
+
+// Represents the intentional absence of any object value.
+var x = null;
+console.log(typeof x);
+
+var y = undefined;
+console.log(typeof y);
+
+var isHappy = true;
+console.log(typeof isHappy);
+
+const student = {
+    fullName : "Rahul Kumar",
+    age : 20,
+    cgpa : 3.96,
+    isPass : true,
+};
+
+console.log(student);
+student.age += 1;
+console.log(student.age);
---
+++ chapter - 2/conditional.js
@@ -0,0 +1,45 @@
+// This is a single line comment 
+/**
+ * This is used to create multiline comment
+ */
+
+// This Line Prints Hellow World 
+console.log('Hellow World'); 
+
+console.log('Arithematic Operators');
+let a = 6;
+let b = 9;
+let c = 2;
+let d = 10;
+
+console.log('a = ', a, 'b = ', b, 'c = ', c, 'd = ', d);
+console.log(' a + b = ', a + b);
+console.log(' a - b = ', a - b);
+console.log(' a * b = ', a * b);
+console.log(' a / b = ', a / b);
+
+console.log('Modulus Operators : %')
+console.log('a % b =', a % b);
+
+console.log('Exponentiation operator : **');
+console.log('a ** b =', a ** c);
+
+console.log('Increment Operator : (which is known as Unary Operator) : ')
+console.log('++a = ', ++a);
+console.log('c++ = ', c++);
+console.log('c = ', c);
+
+console.log('Decrement Operator : (which is known as Unary Operator) : ')
+console.log('--b = ', --b);
+console.log('d-- =', d--);
+console.log('d = ', d);
+
+// Assignment Operators 
+ a += 4;
+ console.log('a = ', a);
+ a -= 4;
+ console.log('a = ', a);
+ a *= 4;
+ console.log('a = ', a);
+ a /= 4;
+ console.log('a = ', a);
